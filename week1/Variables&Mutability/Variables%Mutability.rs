54cce19185f0cc9033f334a393e17821a657161d
Hurotamo <166716974+Hurotamo@users.noreply.github.com>
2024-11-21 23:13:07 +0800

    Create Variables%Mutability.rs

---
+++ week1/Variables&Mutability/Variables%Mutability.rs
@@ -0,0 +1,8 @@
+fn main() {
+    let x = 5; // Immutable variable
+    println!("The value of x is: {}", x);
+
+    let mut y = 10; // Mutable variable
+    y += 5;
+    println!("The value of y is: {}", y);
+}
