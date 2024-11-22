582ba5e9b6fdcd2ec6a0659f1dd29a1eeb0aa415
Hurotamo <166716974+Hurotamo@users.noreply.github.com>
2024-11-22 22:41:56 +0800

    Create datatypes.rs

---
+++ week1/DataTypes/datatypes.rs
@@ -0,0 +1,8 @@
+fn main() {
+    let int: i32 = 42; // Signed 32-bit integer
+    let float: f64 = 3.14; // 64-bit floating point
+    let boolean: bool = true;
+    let character: char = 'R';
+
+    println!("{}, {}, {}, {}", int, float, boolean, character);
+}
